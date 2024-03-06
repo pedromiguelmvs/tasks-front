84222676e24a77e4e2ee279e5bfc360e09d98595
miguelito ツ <pedromiguelmvs>
2024-03-06 14:15:14 -0300

    chore(env): altera o port da API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   mockApi: false,
-  urlApi: 'http://localhost:5211/api',
-  graphql: 'http://localhost:5211/graphql',
+  urlApi: 'http://localhost:5000/api',
+  graphql: 'http://localhost:5000/graphql',
   viaCepUrl: 'https://viacep.com.br/ws',
 };
